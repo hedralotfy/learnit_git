d776e7ef1213fdb3c9abc6b040a40c29554a3a89
Hedra Lotfy <hedralotfy20@gmail.com>
2024-10-09 20:03:32 +0300

    doing removal

---
+++ src/com/innovator/lotfy/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.innovator.lotfy;
+
+/**
+ * Hello World application
+ *  @author Hedra-Lotfy
+ *  @since 3rd November 2023
+ *  {@code Text}
+ *  @see <a href="https://it-bulls.com/">Visit: IT-bulls</a>
+ */
+public class HelloWorld {
+
+	/**
+	 * This is an entry point to our program.
+	 * 
+	 * @param args input arguments
+	 * @see https://docs.google.com/document/d/1Zh60MWoy3WfDQaUzcw7r0kJIPITbVl0pSdKDFufJ-Fs/edit
+	 * {@inheritDoc}
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
